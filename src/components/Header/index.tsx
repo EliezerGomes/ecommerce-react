f111a10b1cb2d79ae168765fcf0304186737f455
EliezerGomes <eliezerportilho@gmail.com>
2025-01-24 06:15:28 -0300

    ajustes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,29 @@
 import "./index.css";
+import { IoCartOutline } from "react-icons/io5";
+import { LuUser } from "react-icons/lu";
+import { IoIosSearch } from "react-icons/io";
 
 export function Header() {
   return (
     <div className="header-container">
-      <div className="col-width-6">
-        <div className="header-1">
-          <h2 className="title-header">React Marketplace</h2>
-
-          <div>Ínicio</div>
-        </div>
+      <div className="header-1">
+        <h2 className="title-header">React Marketplace</h2>
       </div>
 
-      <div className="col-width-6">bbb</div>
+      <div className="search-component">
+        <IoIosSearch className="search" />
+        <input type="text" placeholder="Buscar..." />
+      </div>
+
+      <div className="header-option">
+        <button>
+          <IoCartOutline className="option" />
+        </button>
+
+        <button>
+          <LuUser className="option" />
+        </button>
+      </div>
     </div>
   );
 }
